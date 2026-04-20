4f9584f8cc5f334bbb069e9a9f9bdc708a221e59
5885885 <isak_wiklund@hotmail.com>
2026-04-20 23:58:50 +0200

    Add files via upload
    Small bug fix

--- ScriptPropertyPrinter/ScriptPropertyPrinter.gd
+++ ScriptPropertyPrinter/ScriptPropertyPrinter.gd
@@ -3,15 +3,18 @@ extends Node
 
 # Can be used to print properties for nodes, to track progress and avoid getting drowned
 # in prints such as when using the _process function for prints.
-# Can not reliably access preexisting properties like process_mode.
-# Can not access properties tied to internal functions. 
+# Can not reliably make correct prints for internal properties.
 # Can access properties defined in scripts, like vars and exports.
 # Can access runtime properties like velocity, state and rotation.
 
 @onready var timer: Timer = $Timer
 
 @export var wait_time: float = 1.0
+
+## StringNames of properties to print. 
 @export var properties_to_print: Array[StringName]
+
+## The node to print the properties for.
 @export var node_to_print_for: Node
 
 func _ready() -> void:
@@ -20,10 +23,9 @@ func _ready() -> void:
 func _on_timer_timeout() -> void:
 	if node_to_print_for != null:
 		for property in properties_to_print:
-			if node_to_print_for.get(property):
+			if node_to_print_for.get(property) != null:
 				print("%s's %s: %s" % [node_to_print_for.name, property, node_to_print_for.get(property)])
 			elif !node_to_print_for.get(property):
-				print("%s either does not have property %s" % [node_to_print_for.name, property])
-				print("or the property is an engine internal property not detected by this tool.")
+				print("Can't find property %s" % [property])
 	else:
-		print("Assign a node to print for.")
+		push_error("Assign a node to print for.")
